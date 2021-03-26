3aa3a028d1607a15a6684fb2aaf1fd9f43e98a16
SuperBunika <marko@babuncic.com>
2021-03-26 12:05:29 +0100

    treci zadatak using dictionary

--- zadatak_3.py
+++ zadatak_3.py
@@ -1,6 +1,13 @@
 datoteka = input("Unesite naziv datoteke: ")
 ekstenzija = datoteka[datoteka.rfind("."):]
 
+ekstenzije = {
+    ".py": "Python source kod",
+    ".txt": "tekstualna datoteka",
+    ".php": "PHP source kod"
+}
+
+"""
 if ekstenzija == ".py":
     print("Vaša datoteka ima ekstenziju %s i to je Python source kod." % ekstenzija)
 elif ekstenzija == ".txt":
@@ -9,5 +16,9 @@ elif ekstenzija == ".php":
     print("Vaša datoteka ima ekstenziju %s i to je PHP source kod." % ekstenzija)
 else:
     print("Ne prepoznajem ekstenziju.")
+"""
 
-#print(ekstenzija)
+if ekstenzija in ekstenzije:
+    print("Vaša datoteka ima ekstenziju {} i to je {}.".format(ekstenzija, ekstenzije[ekstenzija]))
+else:
+    print("Ne prepoznajem ekstenziju.")
